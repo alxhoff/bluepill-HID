48dce3ce4645bcb90af6aa14a7d9e27354353532
Alex Hoffman <alxhoff@gmail.com>
2020-04-06 22:17:05 +0200

    CMake bluepill HID example

---
+++ cmake/build/CMakeFiles/STM32F1_bluepill.elf.dir/DependInfo.cmake
@@ -0,0 +1,96 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/home/alxhoff/git/GitHub/bluepill-HID/cmake/lib/STM32F1-Cube-Firmware/Drivers/CMSIS/Device/ST/STM32F1xx/Source/Templates/gcc/startup_stm32f103xb.s" "/home/alxhoff/git/GitHub/bluepill-HID/cmake/build/CMakeFiles/STM32F1_bluepill.elf.dir/lib/STM32F1-Cube-Firmware/Drivers/CMSIS/Device/ST/STM32F1xx/Source/Templates/gcc/startup_stm32f103xb.s.o"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "STM32F1"
+  "STM32F103xB"
+  "USE_FULL_LL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "../"
+  "../Inc"
+  "../lib/STM32F1-Cube-Firmware/Middlewares/Third_Party/FreeRTOS/Source/include"
+  "../lib/STM32F1-Cube-Firmware/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS"
+  "../lib/STM32F1-Cube-Firmware/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM3"
+  "../lib/STM32F1-Cube-Firmware/Drivers/STM32F1xx_HAL_Driver/Inc"
+  "../lib/STM32F1-Cube-Firmware/Drivers/CMSIS/Device/ST/STM32F1xx/Include"
+  "../lib/STM32F1-Cube-Firmware/Drivers/CMSIS/Include"
+  "../lib/STM32F1-Cube-Firmware/Middlewares/ST/STM32_USB_Device_Library/Core/Inc"
+  "../lib/STM32F1-Cube-Firmware/Middlewares/ST/STM32_USB_Device_Library/Class/HID/Inc"
+  )
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/alxhoff/git/GitHub/bluepill-HID/cmake/Src/freertos.c" "/home/alxhoff/git/GitHub/bluepill-HID/cmake/build/CMakeFiles/STM32F1_bluepill.elf.dir/Src/freertos.c.o"
+  "/home/alxhoff/git/GitHub/bluepill-HID/cmake/Src/main.c" "/home/alxhoff/git/GitHub/bluepill-HID/cmake/build/CMakeFiles/STM32F1_bluepill.elf.dir/Src/main.c.o"
+  "/home/alxhoff/git/GitHub/bluepill-HID/cmake/Src/stm32f1xx_hal_msp.c" "/home/alxhoff/git/GitHub/bluepill-HID/cmake/build/CMakeFiles/STM32F1_bluepill.elf.dir/Src/stm32f1xx_hal_msp.c.o"
+  "/home/alxhoff/git/GitHub/bluepill-HID/cmake/Src/stm32f1xx_it.c" "/home/alxhoff/git/GitHub/bluepill-HID/cmake/build/CMakeFiles/STM32F1_bluepill.elf.dir/Src/stm32f1xx_it.c.o"
+  "/home/alxhoff/git/GitHub/bluepill-HID/cmake/Src/usb_device.c" "/home/alxhoff/git/GitHub/bluepill-HID/cmake/build/CMakeFiles/STM32F1_bluepill.elf.dir/Src/usb_device.c.o"
+  "/home/alxhoff/git/GitHub/bluepill-HID/cmake/Src/usbd_conf.c" "/home/alxhoff/git/GitHub/bluepill-HID/cmake/build/CMakeFiles/STM32F1_bluepill.elf.dir/Src/usbd_conf.c.o"
+  "/home/alxhoff/git/GitHub/bluepill-HID/cmake/Src/usbd_desc.c" "/home/alxhoff/git/GitHub/bluepill-HID/cmake/build/CMakeFiles/STM32F1_bluepill.elf.dir/Src/usbd_desc.c.o"
+  "/home/alxhoff/git/GitHub/bluepill-HID/cmake/lib/STM32F1-Cube-Firmware/Drivers/CMSIS/Device/ST/STM32F1xx/Source/Templates/system_stm32f1xx.c" "/home/alxhoff/git/GitHub/bluepill-HID/cmake/build/CMakeFiles/STM32F1_bluepill.elf.dir/lib/STM32F1-Cube-Firmware/Drivers/CMSIS/Device/ST/STM32F1xx/Source/Templates/system_stm32f1xx.c.o"
+  "/home/alxhoff/git/GitHub/bluepill-HID/cmake/lib/STM32F1-Cube-Firmware/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c" "/home/alxhoff/git/GitHub/bluepill-HID/cmake/build/CMakeFiles/STM32F1_bluepill.elf.dir/lib/STM32F1-Cube-Firmware/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c.o"
+  "/home/alxhoff/git/GitHub/bluepill-HID/cmake/lib/STM32F1-Cube-Firmware/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c" "/home/alxhoff/git/GitHub/bluepill-HID/cmake/build/CMakeFiles/STM32F1_bluepill.elf.dir/lib/STM32F1-Cube-Firmware/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c.o"
+  "/home/alxhoff/git/GitHub/bluepill-HID/cmake/lib/STM32F1-Cube-Firmware/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c" "/home/alxhoff/git/GitHub/bluepill-HID/cmake/build/CMakeFiles/STM32F1_bluepill.elf.dir/lib/STM32F1-Cube-Firmware/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c.o"
+  "/home/alxhoff/git/GitHub/bluepill-HID/cmake/lib/STM32F1-Cube-Firmware/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c" "/home/alxhoff/git/GitHub/bluepill-HID/cmake/build/CMakeFiles/STM32F1_bluepill.elf.dir/lib/STM32F1-Cube-Firmware/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c.o"
+  "/home/alxhoff/git/GitHub/bluepill-HID/cmake/lib/STM32F1-Cube-Firmware/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pcd.c" "/home/alxhoff/git/GitHub/bluepill-HID/cmake/build/CMakeFiles/STM32F1_bluepill.elf.dir/lib/STM32F1-Cube-Firmware/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pcd.c.o"
+  "/home/alxhoff/git/GitHub/bluepill-HID/cmake/lib/STM32F1-Cube-Firmware/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pcd_ex.c" "/home/alxhoff/git/GitHub/bluepill-HID/cmake/build/CMakeFiles/STM32F1_bluepill.elf.dir/lib/STM32F1-Cube-Firmware/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pcd_ex.c.o"
+  "/home/alxhoff/git/GitHub/bluepill-HID/cmake/lib/STM32F1-Cube-Firmware/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c" "/home/alxhoff/git/GitHub/bluepill-HID/cmake/build/CMakeFiles/STM32F1_bluepill.elf.dir/lib/STM32F1-Cube-Firmware/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c.o"
+  "/home/alxhoff/git/GitHub/bluepill-HID/cmake/lib/STM32F1-Cube-Firmware/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c" "/home/alxhoff/git/GitHub/bluepill-HID/cmake/build/CMakeFiles/STM32F1_bluepill.elf.dir/lib/STM32F1-Cube-Firmware/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c.o"
+  "/home/alxhoff/git/GitHub/bluepill-HID/cmake/lib/STM32F1-Cube-Firmware/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c" "/home/alxhoff/git/GitHub/bluepill-HID/cmake/build/CMakeFiles/STM32F1_bluepill.elf.dir/lib/STM32F1-Cube-Firmware/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c.o"
+  "/home/alxhoff/git/GitHub/bluepill-HID/cmake/lib/STM32F1-Cube-Firmware/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_ll_pwr.c" "/home/alxhoff/git/GitHub/bluepill-HID/cmake/build/CMakeFiles/STM32F1_bluepill.elf.dir/lib/STM32F1-Cube-Firmware/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_ll_pwr.c.o"
+  "/home/alxhoff/git/GitHub/bluepill-HID/cmake/lib/STM32F1-Cube-Firmware/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_ll_rcc.c" "/home/alxhoff/git/GitHub/bluepill-HID/cmake/build/CMakeFiles/STM32F1_bluepill.elf.dir/lib/STM32F1-Cube-Firmware/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_ll_rcc.c.o"
+  "/home/alxhoff/git/GitHub/bluepill-HID/cmake/lib/STM32F1-Cube-Firmware/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_ll_usb.c" "/home/alxhoff/git/GitHub/bluepill-HID/cmake/build/CMakeFiles/STM32F1_bluepill.elf.dir/lib/STM32F1-Cube-Firmware/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_ll_usb.c.o"
+  "/home/alxhoff/git/GitHub/bluepill-HID/cmake/lib/STM32F1-Cube-Firmware/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_ll_utils.c" "/home/alxhoff/git/GitHub/bluepill-HID/cmake/build/CMakeFiles/STM32F1_bluepill.elf.dir/lib/STM32F1-Cube-Firmware/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_ll_utils.c.o"
+  "/home/alxhoff/git/GitHub/bluepill-HID/cmake/lib/STM32F1-Cube-Firmware/Middlewares/ST/STM32_USB_Device_Library/Class/HID/Src/usbd_hid.c" "/home/alxhoff/git/GitHub/bluepill-HID/cmake/build/CMakeFiles/STM32F1_bluepill.elf.dir/lib/STM32F1-Cube-Firmware/Middlewares/ST/STM32_USB_Device_Library/Class/HID/Src/usbd_hid.c.o"
+  "/home/alxhoff/git/GitHub/bluepill-HID/cmake/lib/STM32F1-Cube-Firmware/Middlewares/ST/STM32_USB_Device_Library/Core/Src/usbd_core.c" "/home/alxhoff/git/GitHub/bluepill-HID/cmake/build/CMakeFiles/STM32F1_bluepill.elf.dir/lib/STM32F1-Cube-Firmware/Middlewares/ST/STM32_USB_Device_Library/Core/Src/usbd_core.c.o"
+  "/home/alxhoff/git/GitHub/bluepill-HID/cmake/lib/STM32F1-Cube-Firmware/Middlewares/ST/STM32_USB_Device_Library/Core/Src/usbd_ctlreq.c" "/home/alxhoff/git/GitHub/bluepill-HID/cmake/build/CMakeFiles/STM32F1_bluepill.elf.dir/lib/STM32F1-Cube-Firmware/Middlewares/ST/STM32_USB_Device_Library/Core/Src/usbd_ctlreq.c.o"
+  "/home/alxhoff/git/GitHub/bluepill-HID/cmake/lib/STM32F1-Cube-Firmware/Middlewares/ST/STM32_USB_Device_Library/Core/Src/usbd_ioreq.c" "/home/alxhoff/git/GitHub/bluepill-HID/cmake/build/CMakeFiles/STM32F1_bluepill.elf.dir/lib/STM32F1-Cube-Firmware/Middlewares/ST/STM32_USB_Device_Library/Core/Src/usbd_ioreq.c.o"
+  "/home/alxhoff/git/GitHub/bluepill-HID/cmake/lib/STM32F1-Cube-Firmware/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS/cmsis_os.c" "/home/alxhoff/git/GitHub/bluepill-HID/cmake/build/CMakeFiles/STM32F1_bluepill.elf.dir/lib/STM32F1-Cube-Firmware/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS/cmsis_os.c.o"
+  "/home/alxhoff/git/GitHub/bluepill-HID/cmake/lib/STM32F1-Cube-Firmware/Middlewares/Third_Party/FreeRTOS/Source/croutine.c" "/home/alxhoff/git/GitHub/bluepill-HID/cmake/build/CMakeFiles/STM32F1_bluepill.elf.dir/lib/STM32F1-Cube-Firmware/Middlewares/Third_Party/FreeRTOS/Source/croutine.c.o"
+  "/home/alxhoff/git/GitHub/bluepill-HID/cmake/lib/STM32F1-Cube-Firmware/Middlewares/Third_Party/FreeRTOS/Source/event_groups.c" "/home/alxhoff/git/GitHub/bluepill-HID/cmake/build/CMakeFiles/STM32F1_bluepill.elf.dir/lib/STM32F1-Cube-Firmware/Middlewares/Third_Party/FreeRTOS/Source/event_groups.c.o"
+  "/home/alxhoff/git/GitHub/bluepill-HID/cmake/lib/STM32F1-Cube-Firmware/Middlewares/Third_Party/FreeRTOS/Source/list.c" "/home/alxhoff/git/GitHub/bluepill-HID/cmake/build/CMakeFiles/STM32F1_bluepill.elf.dir/lib/STM32F1-Cube-Firmware/Middlewares/Third_Party/FreeRTOS/Source/list.c.o"
+  "/home/alxhoff/git/GitHub/bluepill-HID/cmake/lib/STM32F1-Cube-Firmware/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM3/port.c" "/home/alxhoff/git/GitHub/bluepill-HID/cmake/build/CMakeFiles/STM32F1_bluepill.elf.dir/lib/STM32F1-Cube-Firmware/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM3/port.c.o"
+  "/home/alxhoff/git/GitHub/bluepill-HID/cmake/lib/STM32F1-Cube-Firmware/Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_4.c" "/home/alxhoff/git/GitHub/bluepill-HID/cmake/build/CMakeFiles/STM32F1_bluepill.elf.dir/lib/STM32F1-Cube-Firmware/Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_4.c.o"
+  "/home/alxhoff/git/GitHub/bluepill-HID/cmake/lib/STM32F1-Cube-Firmware/Middlewares/Third_Party/FreeRTOS/Source/queue.c" "/home/alxhoff/git/GitHub/bluepill-HID/cmake/build/CMakeFiles/STM32F1_bluepill.elf.dir/lib/STM32F1-Cube-Firmware/Middlewares/Third_Party/FreeRTOS/Source/queue.c.o"
+  "/home/alxhoff/git/GitHub/bluepill-HID/cmake/lib/STM32F1-Cube-Firmware/Middlewares/Third_Party/FreeRTOS/Source/tasks.c" "/home/alxhoff/git/GitHub/bluepill-HID/cmake/build/CMakeFiles/STM32F1_bluepill.elf.dir/lib/STM32F1-Cube-Firmware/Middlewares/Third_Party/FreeRTOS/Source/tasks.c.o"
+  "/home/alxhoff/git/GitHub/bluepill-HID/cmake/lib/STM32F1-Cube-Firmware/Middlewares/Third_Party/FreeRTOS/Source/timers.c" "/home/alxhoff/git/GitHub/bluepill-HID/cmake/build/CMakeFiles/STM32F1_bluepill.elf.dir/lib/STM32F1-Cube-Firmware/Middlewares/Third_Party/FreeRTOS/Source/timers.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "STM32F1"
+  "STM32F103xB"
+  "USE_FULL_LL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../"
+  "../Inc"
+  "../lib/STM32F1-Cube-Firmware/Middlewares/Third_Party/FreeRTOS/Source/include"
+  "../lib/STM32F1-Cube-Firmware/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS"
+  "../lib/STM32F1-Cube-Firmware/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM3"
+  "../lib/STM32F1-Cube-Firmware/Drivers/STM32F1xx_HAL_Driver/Inc"
+  "../lib/STM32F1-Cube-Firmware/Drivers/CMSIS/Device/ST/STM32F1xx/Include"
+  "../lib/STM32F1-Cube-Firmware/Drivers/CMSIS/Include"
+  "../lib/STM32F1-Cube-Firmware/Middlewares/ST/STM32_USB_Device_Library/Core/Inc"
+  "../lib/STM32F1-Cube-Firmware/Middlewares/ST/STM32_USB_Device_Library/Class/HID/Inc"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
